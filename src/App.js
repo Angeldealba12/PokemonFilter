ed387dc7a735fb6f9284e6219ca20d34ebae5732
Angel <angel.dealba.garcia@gmail.com>
2021-07-19 20:45:38 -0500

    seach box with pokeApi

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import Pokemon from './Pokemon';
 
 function App() {
+
+
+  const [searchTarget, setSearchTarget] = useState('1')
+  const [name, setName] = useState('')
+  const [img, setImg] = useState('')
+
+
+  useEffect(() => {
+
+      if(searchTarget){
+        const logic = async () => {
+          const url = `https://pokeapi.co/api/v2/pokemon/${searchTarget.toLocaleLowerCase()}`
+    
+    
+          const res = await fetch(url).then(response => response.json())
+          setName(res.name)
+          setImg(res.sprites.front_default)
+          
+        }
+        logic()
+      }
+
+  }, [searchTarget])
+
+ 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input value={searchTarget} onChange={(e) => setSearchTarget(e.target.value)}/>
+        <button onClick={() => setSearchTarget(searchTarget)}>Buscar</button>
+        <Pokemon name={name.toLocaleUpperCase()} img={img}/>
       </header>
     </div>
   );
---
+++ src/Pokemon.js
@@ -0,0 +1,13 @@
+
+const Pokemon = ( { name, img } ) => {
+
+    return(
+        <div>
+            <h3>{name}</h3>
+        <img src={img} alt={name}/>
+        </div>
+    )
+
+}
+
+export default Pokemon;
